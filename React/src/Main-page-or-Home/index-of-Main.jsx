1fec22dd0be547b802de806a2e8ef3d273272b1a
farhanalimohammadi <farhanmohammadi045@gmail.com>
2024-12-14 20:57:45 -0800

    commit for create Main page or home and I created Header  of page . from farhan

---
+++ React/src/Main-page-or-Home/index-of-Main.jsx
@@ -0,0 +1,11 @@
+import React,{useState , useEffect} from 'react';
+import Header2 from '../staticElems/main-header/header-2';
+export default function Homepage(){
+
+    return(
+        <div className=''>
+            {/* header */}
+            <Header2/>
+        </div>
+    )
+}
--- React/src/Main.js
+++ React/src/Main.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './index.css';
 import FreeShoppingPage from './Free-Shopping-pege/index-of-page';
 import ShoppingPage from './Shopping-page/index-of-Shopping-page'
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+import Homepage from './Main-page-or-Home/index-of-Main';
+// import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 
 
 export default function MainApp(){
     //این صفحه هنوز تکمبل نشده و عمل روتینگ هنوز اعمال نشده جهت اضافه کردن صفحه جدید کامپونتت قبلی را کامنت کنید 
     return(
-        <div className='bg-blue-400 min-h-screen box-border m-0 p-0'>
-            <ShoppingPage/>
+        <div className='bg-componentBg-primeryBg min-h-screen box-border m-0 p-0'>
+            <Homepage/>
+            {/* <ShoppingPage/> */}
+            {/* <FreeShoppingPage/> */}
         </div>
     )
 }
