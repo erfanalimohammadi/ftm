e94b9346d536707b839a2e31bca66afc7733fa6c
farhanalimohammadi <farhanmohammadi045@gmail.com>
2024-12-31 00:22:04 -0800

    commit fir complate the main page from Farhan

--- React/src/Main-page-or-Home/componets/fourth-part/Comments-Slider/Slider.jsx
+++ React/src/Main-page-or-Home/componets/fourth-part/Comments-Slider/Slider.jsx
@@ -1,58 +1,113 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
+import Slides from './Slids';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+
+// import required modules
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import { comment } from 'postcss';
+
+//هنوز تکمیل نشده ....
 
 export default function Slider() {
 
-  return (
-    <div className="w-[100%] flex flex-row justify-center items-center">
+    const bigComments = [
+            {name : 'Farhan' , stars : [1, 2, 3, 4, 5] , comment : "You have people very nice. I had a problemei my account, I did something wrong and I could't log în just after I talked to them. Very understanding people, they answered very fast and very clear. Thank you for your services."},
 
-        <div className="w-[1376px] flex flex-row justify-between items-center absolute right-auto left-auto translate-y-[0px]">
-                <button
-                    className='border-none outline-none cursor-pointer'>
+            {name : 'Erfan' , stars : [1, 2, 3] , comment : "good and reliable company, payments are made, good conditions for trading, excellent support work"},
+
+            {name : 'Asgar' , stars : [1, 2] , comment : "You have people very nice. I had a problemei my account, I did something wrong and I could't log în just after I talked to them. Very understanding people, they answered very fast and very clear. Thank you for your services."},
+
+            {name : 'Farhan2' , stars : [1, 2, 3, 4, 5] , comment : "They are great fast payment was done refund was next day really happy with them"},
+            
+            {name : 'Asgar' , stars : [1, 2] , comment : "You have people very nice. I had a problemei my account, I did something wrong and I could't log în just after I talked to them. Very understanding people, they answered very fast and very clear. Thank you for your services."},
+    ]
+    const smallComments = [
+                {name : 'Ali' , stars : [1, 2, 3, 4 ,5] , comment : "They are great fast payment was done refund was next day really happy with them"}, 
+    
+                {name : 'Mohammad' , stars : [1, 2, 3, 4, 5] , comment : "It was my second pay out, and everything cool"},
+    
+                {name : 'Soheil' , stars : [1, 2, 3, 4] , comment : "They are great fast payment was done refund was next day really happy with them"},
+    
+                {name : 'Farhan3' , stars : [1, 2, 3, 4, 5] , comment : "You have people very nice. I had a problemei my account, I did something wrong and I could't log în just after I talked to them. Very understanding people, they answered very fast and very clear. Thank you for your services."},
+    
+                {name : 'Farhan4' , stars : [1] , comment : "They are great fast payment was done refund was next day really happy with them"},
+    ]
+    const combinedComments = bigComments.map((big, index) => ({
+        big,
+        small: smallComments[index], // مطمئن شوید که تعداد کامنت‌های کوچک به‌اندازه کامنت‌های بزرگ باشد
+      }));
+
+
+
+  return (
+    <div className="relative w-full flex flex-col justify-center items-center">
+      <Swiper
+        slidesPerView='auto' 
+        centeredSlides={true} 
+        spaceBetween={16}
+        loop={true}
+        navigation={{
+          prevEl: ".custom-prev",
+          nextEl: ".custom-next",
+        }}
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination]}
+        className="w-full h-[444px]"
+      >
+        {combinedComments.map((item, index) => (
+          <SwiperSlide
+            key={index}
+            style={{width: '296px '}}
+            className="w-full h-full flex flex-col gap-y-4 justify-center items-center rounded-lg"
+          >
+
+
+              <Slides
+                name={item.big.name}
+                stars={item.big.stars}
+                comment={item.big.comment}
+              />
+            {item.small && (
+                <Slides
+                  name={item.small.name}
+                  stars={item.small.stars}
+                  comment={item.small.comment}
+                />
+            )}
+          </SwiperSlide>
+        ))}
+
+      </Swiper>
+      <div className="w-[1376px] flex flex-row justify-between items-center absolute right-auto left-auto translate-y-[0px]">  
+                    <button
+                    className='custom-prev border-none outline-none cursor-pointer'>
                         <svg className='cursor-pointer' width="21" height="42" viewBox="0 0 21 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18.9983 39.4795L3.785 24.2662C1.98833 22.4695 1.98833 19.5295 3.785 17.7329L18.9983 2.51953" stroke="#1481FE" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                     </button>
     
                 <button
-                    className='border-none outline-none cursor-pointer'>
+                    className=' custom-next border-none outline-none cursor-pointer'>
                         <svg className='cursor-pointer rotate-180' width="21" height="42" viewBox="0 0 21 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18.9983 39.4795L3.785 24.2662C1.98833 22.4695 1.98833 19.5295 3.785 17.7329L18.9983 2.51953" stroke="#1481FE" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                </button>
-        </div>
+                    </button>
+    
+    
+      </div>
 
-        <div className="scroll-container overflow-y-hidden">
-            <div className="scroll-item h-full inline-flex flex-col justify-center items-center">
-                <div className="w-full h-[40%] bg-btnColors-Mailblue rounded-xl"></div>
-                <div className="w-full h-[60%] bg-btnColors-Mailblue rounded-xl"></div>
-            </div>
-            <div className="scroll-item h-full inline-flex flex-col justify-center items-center">
-                <div className="w-full h-[65%] bg-btnColors-Mailblue rounded-xl"></div>
-                <div className="w-full h-[35%] bg-btnColors-Mailblue rounded-xl"></div>
-            </div>
-            <div className="scroll-item h-full inline-flex flex-col justify-center items-center">
-                <div className="w-full h-[25%] bg-btnColors-Mailblue rounded-xl"></div>
-                <div className="w-full h-[75%] bg-btnColors-Mailblue rounded-xl"></div>
-            </div>
-            <div className="scroll-item h-full inline-flex flex-col justify-center items-center">
-                <div className="w-full h-[70%] bg-btnColors-Mailblue rounded-xl"></div>
-                <div className="w-full h-[30%] bg-btnColors-Mailblue rounded-xl"></div>
-            </div>
-            <div className="w-[296px] h-full inline-flex flex-col justify-center items-center">
-                <div className="w-full h-[40%] bg-btnColors-Mailblue rounded-xl"></div>
-                <div className="w-full h-[60%] bg-btnColors-Mailblue rounded-xl"></div>
-            </div>
-            <div className="w-[296px] h-full inline-flex flex-col justify-center items-center">
-                <div className="w-full h-[60%] bg-btnColors-Mailblue rounded-xl"></div>
-                <div className="w-full h-[40%] bg-btnColors-Mailblue rounded-xl"></div>
-            </div>
-            <div className="scroll-item h-full inline-flex flex-col justify-center items-center">
-                <div className="w-full h-[45%] bg-btnColors-Mailblue rounded-xl"></div>
-                <div className="w-full h-[55%] bg-btnColors-Mailblue rounded-xl"></div>
-            </div>
-        </div>
     </div>
+    
   );
 }
+
+
 
 
