816dbcef7d1b60bdc4a4a9c3e92110ebd809d268
farhanalimohammadi <farhanmohammadi045@gmail.com>
2025-01-06 22:39:00 -0800

    commit for complate a part of the Analiys page from Farhan

--- React/src/Analisys-page/index-of-AnalisysPage.jsx
+++ React/src/Analisys-page/index-of-AnalisysPage.jsx
@@ -6,6 +6,9 @@ import Account from "./componets/Account";
 import Buttons from "./componets/Buttons";
 import Trial from "./componets/Free-Trial";
 import AnalisysChart from './componets/Chart'
+import LineChart from "./componets/Chart2";
+import Objectives from "./componets/Objective";
+import Options from "./componets/Options";
 export default function Analisys(){
 
     return (
@@ -14,14 +17,19 @@ export default function Analisys(){
             <div className="flex flex-row w-full min-h-screen">
                 <Sidebar/>
 
-                <div className="flex flex-col justify-start items-start mt-topSpace w-[66%] ml-[360px] gap-y-[16px]">
-                    <RouterElement />
-                    <Account/>
-                    <Buttons/>
-                    <div className="w-full flex flex-row justify-between items-start gap-x-[32px]">
-                        <AnalisysChart/> 
-                        <Trial/>
+                <div className="flex flex-col justify-start items-start mt-topSpace w-[66%] ml-[360px] gap-y-[48px]">
+                    <div className="w-full flex flex-col justify-start items-start gap-y-[22px]">
+                        <RouterElement/>
+                        <Account/>
+                        <Buttons/>
+                        <div className="w-full flex flex-row justify-between items-start gap-x-[32px]">
+                            <AnalisysChart/> 
+                            <Trial/>
+                        </div>
                     </div>
+                    <LineChart/>
+                    <Objectives/>
+                    <Options name={['Trading Journal' , 'Mentor App' , 'Economic Calendar', 'Useful Links']} />
                 </div>
             </div>
         </div>
